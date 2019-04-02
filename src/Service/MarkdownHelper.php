2087ec1e607c1d6a29b36d4fed96492a9f638fdc
Anderson.Holguin <anderson.holguin@umb.edu.co>
2019-04-02 16:48:35 -0500

    Creating a new channel for logging

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -3,20 +3,31 @@
 
 namespace App\Service;
 use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class MarkdownHelper
 {
     private $cache;
     private $markdown;
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown)
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $logger)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
+        $this->logger = $logger;
     }
 
     public function parse(string $source): string
     {
+        if (stripos($source, 'metus ') !== false) {
+            $this->logger->info('They are talking about metus  again!');
+        }
+
         $item = $this->cache->getItem('aaaa_'.md5($source));
         if(!$item->isHit()){
             $item->set($this->markdown->transform($source));
