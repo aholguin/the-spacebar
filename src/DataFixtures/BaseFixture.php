363e095766ce6fc2583b9fbfc160e1925b2fe3ff
Anderson.Holguin <anderson.holguin@umb.edu.co>
2019-05-31 14:53:14 -0500

    Generating fake data with faker bundle for dev enviroment

--- src/DataFixtures/BaseFixture.php
+++ src/DataFixtures/BaseFixture.php
@@ -7,17 +7,21 @@ namespace App\DataFixtures;
 use App\Entity\Article;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 abstract class BaseFixture extends Fixture
 {
     /** @var ObjectManager */
     private $manager;
+    /** @var Generator */
+    protected $faker;
 
     abstract protected function loadData(ObjectManager $manager);
 
     public function load(ObjectManager $manager)
     {
         $this->manager = $manager;
+        $this->faker = Factory::create();
 
         $this->loadData($manager);
 
